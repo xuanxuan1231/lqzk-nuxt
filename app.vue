610409119c9f5f741fe7ef87113ecc6493496090
chenwenxuan <1763516926@qq.com>
2023-05-03 17:04:47 +0800

    test

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+     <nuxt-content :document="page" />
   </div>
 </template>
+
+<script>
+   export default {
+       async asyncData ({ $content }) {
+           const page = await $content('test').fetch()
+
+           return {
+               page
+           }
+       },
+   }
+</script>
